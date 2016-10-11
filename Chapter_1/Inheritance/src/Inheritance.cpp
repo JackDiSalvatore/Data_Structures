d1c589ea9ed3af5b587c185b831db768aa186c83
jackdisalvatore <jdisalvatore7@gmail.com>
2016-10-10 21:05:55 -0400

    finishing exampled

--- Chapter_1/Inheritance/src/Inheritance.cpp
+++ Chapter_1/Inheritance/src/Inheritance.cpp
@@ -33,15 +33,67 @@ private:
 };
 
 
-class DerivedLevel1 : public virtual BaseClass
+class Derived1Level1 : public virtual BaseClass
 {
 public:
+	void f(char *s = "unknown")
+	{
+		cout << "Function f() in Derived1Level1 called from " << s << endl;
+		g("Derived1Level1");
+		h("Derived1Level1");
+	}
+	void h(char *s = "unknown")
+	{
+		cout << "Function h() from DerivedLevel1 called from " << s << endl;
+	}
 
+};
+
+
+class Derived2Level1 : public virtual BaseClass
+{
+public:
+	void f(char *s = "unknown")
+	{
+		cout << "Function f() from Derived2Level1 called from " << s << endl;
+		g("Derived2Level1");
+//		h();		// error h() is private to BaseClass
+	}
+};
+
+
+class DerivedLevel2 : public Derived1Level1, public Derived2Level1
+{
+public:
+	void f(char *s = "unknown")
+	{
+		cout << "Function f() in DerivedLevel2 called from " << s << endl;
+		g("DerivedLevel2");
+		Derived1Level1::h("DerivedLevel2");
+		BaseClass::f("DerivedLevel2");
+	}
 };
 
 
 int main()
 {
+	/* Sample Program */
+	BaseClass bc;
+	Derived1Level1 d1l1;
+	Derived2Level1 d2l1;
+	DerivedLevel2 dl2;
+
+	bc.f("main(1)");
+//	bc.g();				// g() is not public
+//	bc.h();				// h() is not public
+	d1l1.f("main(2)");
+//	d1l1.g();			// g() not derived as public
+	d1l1.h("main(3)");
+	d2l1.f("main(4)");
+
+
+	dl2.f("main(5");
+	dl2.h();
 
 	return 0;
 }
