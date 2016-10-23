2ac8ee72b7ba3476d2cc58a2b0e834e801b7031c
jackdisalvatore <jdisalvatore7@gmail.com>
2016-10-23 12:34:58 -0400

    overload operators in <functional>
    Third example not working

--- Chapter_1/1-7-4_Function_Objects/src/1-7-4_Function_Objects.cpp
+++ Chapter_1/1-7-4_Function_Objects/src/1-7-4_Function_Objects.cpp
@@ -9,7 +9,7 @@
 #include <iostream>
 using namespace std;
 
-
+/************* Overloading function call operator ***************/
 class classf
 {
 public:
@@ -32,14 +32,69 @@ double sum2(classf f, int n, int m)
 }
 
 
+/********* without overloading function call operator ***********/
+class classf2
+{
+public:
+	classf2() { }
+	double run (double x)
+	{
+		return 2*x;
+	}
+};
+
+
+double sum3(classf2 f, int n, int m)
+{
+	double result = 0;
+	for(int i = n; i <= m; i ++)
+	{
+		result += f.run(i);
+	}
+	return result;
+}
+
+
+/************** Passing operators **************/
+template<class T>
+struct negate : public unary_function<T,T>
+{
+	T operator() (const T& x) const
+	{
+		return -x;
+	}
+};
+
+
+template<class F>
+double sum4(F f, int n, int m)
+{
+	double result = 0;
+	for(int i = n; i <=m; i++)
+	{
+		result += f(i);
+	}
+	return result;
+}
+
+
 int main()
 {
-	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
+	cout << "First Example: " << endl; // prints !!!Hello World!!!
 
 	cout << sum2(classf(),2,5) << endl;
 
 	classf cf;
 	cout << sum2(cf,2,5) << endl;
+
+	cout << "Second Example: \n";
+
+	cout << sum3(classf2(),2,5) << endl;
+
+	cout << "Third Example: \n";
+
+	//cout << sum4(negate<double>(),2,5);
+
 
 	return 0;
 }
