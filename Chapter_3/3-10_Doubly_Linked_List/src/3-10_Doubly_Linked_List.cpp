75fbd39cfa77b7fdb469715ea2bf987b88f812c4
jackdisalvatore <jdisalvatore7@gmail.com>
2017-01-07 19:25:06 -0500

    Doubly Linked list basic interface (api)
    Figure 3.10

--- Chapter_3/3-10_Doubly_Linked_List/src/3-10_Doubly_Linked_List.cpp
+++ Chapter_3/3-10_Doubly_Linked_List/src/3-10_Doubly_Linked_List.cpp
@@ -14,6 +14,6 @@ using namespace std;
 
 
 int main() {
-	cout << "!!!Hello Worlsdfd!!!" << endl; // prints !!!Hello World!!!
+	cout << "!!!Hello World!!" << endl; // prints !!!Hello World!!!
 	return 0;
 }
--- Chapter_3/3-10_Doubly_Linked_List/src/genDDLst.h
+++ Chapter_3/3-10_Doubly_Linked_List/src/genDDLst.h
@@ -13,18 +13,67 @@ template<class T>
 class DDLNode
 {
 public:
-	DDLNode()
-	{
-		next = prev = 0;
-	}
-	DDLNode(const T& el, DDLNode *n = 0, DDLNode *p = 0)
-	{
-		info = el; next = n; prev = p;
-	}
-	T info;
-	DDLNode *next, *prev;
+    DDLNode()
+    {
+        next = prev = 0;
+    }
+    DDLNode(const T& el, DDLNode *n = 0, DDLNode *p = 0)
+    {
+        info = el; next = n; prev = p;
+    }
+private:
+    T info;
+    DDLNode *next, *prev;
 };
 
 
+template<class T>
+class DoublyLinkedList
+{
+public:
+    DoublyLinkedList()
+    {
+        head = tail = 0;
+    }
+    void addToDLLTail(const T&);
+    T deleteFromDLLTail();
+    // Add more Doubly Linked List member functions here!
+protected:
+    DDLNode<T> *head, *tail;
+};
 
-#endif /* DOUBLY_LINKED_LIST */
+
+/* Doubly Linked List Member Functions */
+template<class T>
+void DoublyLinkedList<T>::addToDLLTail(const T& el)
+{
+	if (tail !=0 )
+	{
+		tail = new DDLNode<T>(el, 0, tail);
+		tail->prev->next = tail;
+	}
+	else
+		head = tail = new DDLNode<T>(el);
+}
+
+
+template<class T>
+T DoublyLinkedList<T>::deleteFromDLLTail()
+{
+	T el = tail->info;
+
+	if (head == tail) // If only one node in the list
+	{
+		delete head;
+		head = tail = 0;
+	}
+	else	// If more than one node in the list
+	{
+		tail = tail->prev;
+		delete tail->next;
+		tail->next = 0;
+	}
+
+	return el;
+}
+#endif
