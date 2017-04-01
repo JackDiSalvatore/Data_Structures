f6f09fbd2da9e2804389649aa4ab2e18540335b5
jackdisalvatore <jdisalvatore7@gmail.com>
2017-04-01 16:23:34 -0400

    const - keyword example 2
    const return vale
    const function
    overloading const functions (use const objects!)

--- Chapter_1/const-keyword/main.cpp
+++ Chapter_1/const-keyword/main.cpp
@@ -62,10 +62,34 @@ class Dog {
 	string name;
 public:
 	Dog() { age = 3; name = "Roofus"; }
+
+	// const parameter example
 	void setAge(const int& a) {
 		age = a;
 		// a = 20; const fixes this
 		cout << "Dog's age is " << age << endl;
+	}
+
+	// const return value example
+	// you wanna return by reference so you don't have to make
+	// a copy of the name as you would if you returned a value (it
+	// would be a new value)
+	// const makes sure you cant modify the returned name
+	const string& getName() {return name;}
+
+	// const function: this function will not change any member
+	//                 variables of this class
+	void printDogName() const {
+		cout << name << " -const function"<< endl;
+		// age++;  can't change member variables
+
+		// cout << getName() <<  endl;
+		// cant call other functions that are not const !
+	}
+	// you CAN overload this without the const
+	// this will be invoked when the Dog object is not a const !
+	void printDogName() {
+		cout << getName() << " -non const function" << endl;
 	}
 };
 
@@ -77,6 +101,13 @@ void exp2() {
 	d.setAge(i);
 	cout << i << endl;
 
+	const string& n = d.getName();
+	cout << n << endl;
+
+	d.printDogName();
+
+	const Dog d2;
+	d2.printDogName();
 }
 
 int main(int argc, char* argv[]) {
