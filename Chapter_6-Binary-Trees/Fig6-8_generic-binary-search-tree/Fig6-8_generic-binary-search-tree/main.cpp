b62e4aaef62fa149d81c78fe7d8cea81139a30e7
JackDiSalvatore <jdisalvatore7@gmail.com>
2019-07-27 17:40:55 -0400

    adding git ignore

---
+++ Chapter_6-Binary-Trees/Fig6-8_generic-binary-search-tree/Fig6-8_generic-binary-search-tree/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Fig6-8_generic-binary-search-tree
+//
+//  Created by Jack DiSalvatore on 7/27/19.
+//  Copyright © 2019 Jack DiSalvatore. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
